ffae3e3a7f8a4cddbccd0e644eaa33d13c42ed94
S. Gaurisankar <108788266+S-Gaurisankar@users.noreply.github.com>
2023-05-01 16:48:59 +0530

    Add files via upload

---
+++ Object_detection.py
@@ -0,0 +1,26 @@
+import cv2
+img1 = cv2.imread('assets/Cabs.jpg', 0)
+img2 = cv2.imread('assets/ford.PNG',0)
+img = cv2.resize(img1, (0, 0), fx=0.8, fy=0.8)
+template = cv2.resize(img2, (0, 0), fx=0.8, fy=0.8)
+
+methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
+            cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
+
+h, w = template.shape
+
+for method in methods:
+    img2 = img.copy()
+
+    result = cv2.matchTemplate(img2, template, method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        location = min_loc
+    else:
+        location = max_loc
+
+    bottom_right = (location[0] + w, location[1] + h)    
+    cv2.rectangle(img2, location, bottom_right, 255, 5)
+    cv2.imshow('Match', img2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
